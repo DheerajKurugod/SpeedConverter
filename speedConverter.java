b9b5b1837a65b1e3c025770c1f9f9b48fb4eab8c
Dheeraj Kurugod Raju <dheeraj.kurugod9@gmail.com>
2020-11-19 10:48:28 -0500

    Add files via upload

---
+++ speedConverter.java
@@ -0,0 +1,19 @@
+public class speedConverter {
+    public static long toMilesPerHour (double kiloMetersPerHour){
+
+    if(kiloMetersPerHour<0){
+        return -1;
+    }
+    return Math.round(kiloMetersPerHour/1.609);
+    }
+
+    public static void printConversion (double kiloMetersPerHour){
+
+        if(kiloMetersPerHour<0){
+            System.out.println("Invalid value");
+        }else{
+            long milesPerHour = toMilesPerHour(kiloMetersPerHour);
+            System.out.println(kiloMetersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
